6f05fe5d94d6505c70263081a50c8ce1ce970f98
NikhithaSri4 <155454226+NikhithaSri4@users.noreply.github.com>
2024-02-22 21:05:26 +0530

    Add files via upload

--- 11.9.3.32/codes/plot.py
+++ 11.9.3.32/codes/plot.py
@@ -6,44 +6,43 @@ data = np.loadtxt('values.dat')
 x = data[:, 0]
 y = data[:, 1]
 
-# Plot for values.dat
-plt.figure(figsize=(10, 6))
-plt.subplot(2, 2, 1)
+# Plot for values.dat and save
+plt.figure(figsize=(8, 6))
 plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.title('Plot of f(x) ')
+plt.title('Plot of f(x)')
 plt.grid(True)
-plt.savefig('f1.png', dpi = 300, bbox_inches = 'tight')
+plt.savefig('f1.png')  # Save the plot
+plt.close()  # Close the current figure
 
 # Load data from values1.dat
 data = np.loadtxt('values1.dat')
 n = data[:, 0].astype(int)
 xn = data[:, 1]
 
-# Plot for values1.dat
-plt.subplot(2, 2, 3)
+# Plot for values1.dat and save
+plt.figure(figsize=(8, 6))
 plt.stem(n, xn)
 plt.xlabel('n')
 plt.ylabel('x(n)')
-plt.title('Plot of AP series')
+plt.title('Stem plot of AP series')
 plt.grid(True)
-plt.savefig('ap.png', dpi = 300, bbox_inches = 'tight')
+plt.savefig('ap.png')  # Save the plot
+plt.close()  # Close the current figure
 
 # Load data from values2.dat
 data = np.loadtxt('values2.dat')
 n = data[:, 0].astype(int)
 xn = data[:, 1]
 
-# Plot for values2.dat
-plt.subplot(2, 2, 4)
+# Plot for values2.dat and save
+plt.figure(figsize=(8, 6))
 plt.stem(n, xn)
 plt.xlabel('n')
 plt.ylabel('x(n)')
-plt.title('Plot of GP series ')
+plt.title('Stem plot of GP series')
 plt.grid(True)
-plt.savefig('gp.png', dpi = 300, bbox_inches = 'tight')
-
-plt.tight_layout()
-plt.show()
+plt.savefig('gp.png')  # Save the plot
+plt.close()  # Close the current figure
 
